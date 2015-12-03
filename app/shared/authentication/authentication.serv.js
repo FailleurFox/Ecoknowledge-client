6b1f54b1e4ff16f0c3f0511b1084de7a25681618
Benjamin Benni <benjamin.benni06@gmail.com>
2015-12-03 11:16:31 +0100

    Add hardcoded user ID in Authentication service

--- app/shared/authentication/authentication.serv.js
+++ app/shared/authentication/authentication.serv.js
@@ -9,7 +9,8 @@ ecoknowledgeApp.factory('AuthenticationService', function($cookies){
         },
 
         getUserId : function(){
-            return $cookies.get("EcoknowledgeUserId");
+            return "e3901108-b77d-4412-9cca-39599c90faad";
+            //  FIXME return $cookies.get("EcoknowledgeUserId");
         },
 
         isLoggedIn : function(){
