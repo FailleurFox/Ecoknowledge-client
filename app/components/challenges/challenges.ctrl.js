a338e96bd6262680faef4ef7c6bfd6169acb7982
Hugo Simond <hugosimond@gmail.com>
2015-12-03 10:53:29 +0100

    Auto refresh. Fix le tri par points. Ajout du tri par quantité de badges. Modification des labels des badges

--- app/components/challenges/challenges.ctrl.js
+++ app/components/challenges/challenges.ctrl.js
@@ -1,23 +1,32 @@
 /**
  * Challenges page controller
  */
-challengesMdl.controller('challengesCtrl', function ($scope, $http) {
+challengesMdl.controller('challengesCtrl', function ($scope, $http, $interval) {
     // filter bar variables
     $scope.sortOptions = {
         'nom': 'name',
         'points min.': 'levels[0].points',
-        'points max.': 'levels[challenge.levels.length-1].points',
+        'points max.': 'levels.slice(-1)[0].points',
         'date d\'ouverture': 'startTime',
         'date de fermeture': 'endTime'
     };
     $scope.sortBy = 'name';
 
     // fill the challenges grid
-    // TODO call server
-    $http({
-        method: 'GET',
-        url: '/Ecoknowledge-client/stub-challenges.json'
-    }).success(function (data, status, headers, config) {
-        $scope.challenges = data;
-    });
+    $scope.refresh = function() {
+        // TODO call server
+        $http({
+            method: 'GET',
+            url: '/Ecoknowledge-client/stub-challenges.json'
+        }).success(function (data, status, headers, config) {
+            $scope.challenges = data;
+        });
+    };
+
+    $scope.refresh();
+
+    // Auto refresh
+    $interval(function(){
+        $scope.refresh();
+    }, 10000);
 });
