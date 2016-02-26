8aa093c81cfc341f70771a33c3d6b7aa022df28f
Hugo Simond <hugosimond@gmail.com>
2016-02-26 15:16:34 +0100

    ajout des capteurs et de la confirmation de mot de passe

--- app/src/shared/registration/registration.ctrl.js
+++ app/src/shared/registration/registration.ctrl.js
@@ -2,24 +2,36 @@
  * Registration component controller
  */
 registrationMdl.controller('registrationCtrl', function ($rootScope, $http, $scope, AuthenticationService) {
-    // submit registration information
-    $scope.onSubmit = function(){
+  $scope.captorTypes = ['TMP_CLIM', 'TMP_AMB', 'SWITCH_LIGHT', 'SWITCH_DOOR'];
+  $scope.captors = [];
 
-        $.ajaxPrefilter( "json script", function( options ) {
-            options.crossDomain = true;
-        });
+  // submit registration information
+  $scope.onSubmit = function () {
+    if($scope.password != $scope.confirmPassword)return;
 
-        // TODO add real mapping
-        var data ={
-            "firstName" : $scope.firstName,
-            "lastName" : $scope.lastName,
-            "mail" : $scope.mail,
-            "password" : $scope.password,
-            "symbolicNameToSensorNameMap":{}
-        };
+    $.ajaxPrefilter("json script", function (options) {
+      options.crossDomain = true;
+    });
 
-        $http.post($rootScope.serverURL + 'users', data).success(function (data, status, headers, config) {
-            AuthenticationService.connect(data.id);
-        });
-    }
+    // TODO add real mapping
+    var data = {
+      "firstName": $scope.firstName,
+      "lastName": $scope.lastName,
+      "mail": $scope.mail,
+      "password": $scope.password,
+      "symbolicNameToSensorNameMap": {}
+    };
+
+    $http.post($rootScope.serverURL + 'users', data).success(function (data, status, headers, config) {
+      AuthenticationService.connect(data.id);
+    });
+  };
+
+  $scope.addCaptor = function () {
+    $scope.captors.push({'type': $scope.captorTypes[0], 'name': undefined});
+  };
+
+  $scope.removeCaptor = function (id) {
+    $scope.captors.splice(id, 1);
+  }
 });
